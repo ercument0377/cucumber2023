a21493b4d90d5060f238149a32669280689dc304
ercument0377 <ercument0377@gmail.com>
2023-01-12 13:56:39 +0300

    cucumber

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,32 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+// calıstırmak ıstediğim feature file yi belirtmem lazım
+@CucumberOptions(
+        features = "./src/test/resources/features/firstfeaturefile.feature",  //ZORUNLUDUR  -- dosyanın  pathini girmemiz lazım
+        glue = "stepdefinitions", // ZORUNLUDUR => Stepdefinition adresi veya pathini yazıyoruz
+        tags = "@google_search",  // opsiyoneldir kullanılmayabilir  hangi feature file koşmalı onu belirtmeliyiz -- eger tags kullanmaz isek Runner tum featureleri run eder
+        dryRun = false          //opsiyoneldir yani yazmasakta false olur
+
+)
+
+public class Runner {
+
+}
+/*
+Runner: feature file'lari calistirmak icin kullanilir
+@RunWith : Cucumber.class'lari calistirmak icin kullanilir, kullanmadan Runner calismaz
+@CucumberOptions() : feature path'i eklemek icin kullanilir , step deginition paht, tags, dryRun, report plugins icerir
+ -feature : feature dosyasi path'ini verir, spesifik bir feature file isaret edilebilir
+ -glue : step definition class'inda spesifik path'i isaret eder
+ -tags : hangi feature yada senaryo kosacak bunu belirler. bu tag'i feature file ekliyoruz
+ -dryRun : herhangi bir JAVA kod eksikligini kontol eder (step definition kullaniriz)
+           : dryRun 2 value (deger) alir, true (head-less) veya false
+           : dryRun = false => Herhangi bir JAVA kodunun eksik olup olmadigini kotrol ederken tarayicida testleri calistirir
+           : dryRun = true =>  Herhangi bir JAVA kodunun eksik olup olmadigini kotrol ederken tarayici olmadan testleri calistirir
+ */
